d30b44b21a21c1acf5d21bec552741754d8a0425
Bryan Florestal <61915780+bflorestal@users.noreply.github.com>
2022-01-14 10:15:47 +0100

    Ajout du design

--- views/viewAccueil.php
+++ views/viewAccueil.php
@@ -1,10 +1,15 @@
-<?php
-// utilisé comme nom "$produits" car il est nommé ainsi dans ControllerAccueil.php dans la fonction produits()
-foreach($produits as $produit): ?>
-<h2><?=$produit->nom()." - ".$produit->prix()."€"?></h2>
-<p><?=$produit->type()?></p>
-<img src=<?="'".$produit->img()."'";?>/>
-<a href="/Test">Test</a>
-<hr>
-<?php endforeach;
-?>
+<div class="produits-container">
+    <?php
+    // utilisé comme nom "$produits" car il est nommé ainsi dans ControllerAccueil.php dans la fonction produits()
+    foreach($produits as $produit): ?>
+        <div class="produit-disp">
+            <h2><?=$produit->nom()." - ".$produit->prix()."€"?></h2>
+            <p><?=$produit->type()?></p>
+            <div class="img-container">
+                <img src=<?="'".$produit->img()."'";?>/>
+            </div>
+            <a class="bouton" href="/Test">Test</a>
+        </div>
+    <?php endforeach;
+    ?>
+</div>
